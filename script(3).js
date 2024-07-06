e743c7965380ddcaaeb1da3f7aa9adad2dd5ca8d
nimrasiddiquee <148622732+nimrasiddiquee@users.noreply.github.com>
2024-07-06 08:22:22 -0700

    Add files via upload

---
+++ script(3).js
@@ -0,0 +1,34 @@
+// var email=prompt("nimra@gmail.com")
+
+// if(
+//     email==="nimra@gmail.com"){var password=prompt("nimra")
+//         alert("you are admin")
+//         if(password==="nimra"){
+//             alert("you are nimra")
+//         }
+//     }
+// var schools=["millenniums","allied","horizon","foundation","smart"]
+// var school=prompt("enter your school name")
+
+// if
+// (schools[0]===school){alert("you choose correct")}
+
+//  else if
+// (schools[1]===school){alert("1")}
+// else if
+// (schools[2]===school){alert("2")}
+// else if
+// (schools[3]===school){alert("3")}
+// else if
+// (schools[4]===school){alert("4")}
+// else if
+// (schools[5]===school){alert("5")}
+// else{
+//     alert("no school exit")}
+var foods=["chiness","italian","continental"]
+var food=prompt("enter your items")
+if(foods[0]===food){alert("awesome")}
+else if(foods[1]===food){alert("good")}
+else if(foods[2]===food){alert("not bad")}
+else{ 
+    alert("they all good")}
